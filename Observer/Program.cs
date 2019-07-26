ab5c48e8bf91d0c5327cb37365f8b07dee570b8c
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-07-27 00:41:35 +0600

    Добавьте файлы проекта.

---
+++ Observer/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Observer
+{
+    public interface ISubscriber
+    {
+        void Update(IBublisher subject);
+    }
+
+    public interface IBublisher
+    {
+        
+        void Attach(ISubscriber observer);
+
+        
+        void Detach(ISubscriber observer);
+
+     
+        void Notify();
+    }
+
+    public class Bookstore : IBublisher
+    {
+        public int State { get; set; } = -0;
+
+        private List<ISubscriber> _observers = new List<ISubscriber>();
+
+        public void Attach(ISubscriber observer)
+        {
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
+        }
+
+        public void Detach(ISubscriber observer)
+        {
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Subject: Notifying observers...");
+
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+            }
+        }
+
+     
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("\nSubject: I'm doing something important.");
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("Subject: My state has just changed to: " + this.State);
+            this.Notify();
+        }
+    }
+
+   
+    class BookSubscriber : ISubscriber
+    {
+        public void Update(IBublisher subject)
+        {
+            if ((subject as Bookstore).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+            }
+        }
+    }
+
+    class JournalSubscriber : ISubscriber
+    {
+        public void Update(IBublisher subject)
+        {
+            if ((subject as Bookstore).State == 0 || (subject as Bookstore).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+
+    class ComicsSubscriber : ISubscriber
+    {
+        public void Update(IBublisher subject)
+        {
+            if ((subject as Bookstore).State == 0 || (subject as Bookstore).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            var subject = new Bookstore();
+            var observerA = new BookSubscriber();
+            subject.Attach(observerA);
+
+            var observerB = new JournalSubscriber();
+            subject.Attach(observerB);
+
+            var observerC = new ComicsSubscriber();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+
+namespace Strategy
+{
+    public interface IStrategy
+    {
+        object AlgorithmActions();
+        string Shot();
+        string Jump();
+        string Dodge();
+        string Run();
+    }
+
+    class DragonBattleStrategy : IStrategy
+    {
+        List<string> steps = new List<string>();
+
+        public object AlgorithmActions()
+        {
+            steps.Add(Shot());
+            steps.Add(Jump());
+            steps.Add(Shot());
+            steps.Add(Dodge());
+            steps.Add(Shot());
+            steps.Add(Run());
+            return steps;
+        }
+
+        public string Dodge()
+        {
+            return "уворачиваться от дракона";
+        }
+
+        public string Jump()
+        {
+            return "прыгать в дракона";
+        }
+
+        public string Run()
+        {
+            return "бежать от дракона";
+        }
+
+        public string Shot()
+        {
+            return "стрелять";
+        }
+    }
+
+    class GiantBattleStrategy : IStrategy
+    {
+        List<string> steps = new List<string>();
+
+        public object AlgorithmActions()
+        {
+            steps.Add(Shot());
+            steps.Add(Jump());
+            steps.Add(Shot());
+            steps.Add(Dodge());
+            steps.Add(Shot());
+            steps.Add(Run());
+            return steps;
+        }
+
+        public string Dodge()
+        {
+            return "уворачиваться от гиганта";
+        }
+
+        public string Jump()
+        {
+            return "прыгать в гиганта";
+        }
+
+        public string Run()
+        {
+            return "бежать от гиганта";
+        }
+
+        public string Shot()
+        {
+            return "стрелять в гиганта";
+        }
+    }
+
+    class TurretBattleStrategy : IStrategy
+    {
+        List<string> steps = new List<string>();
+
+        public object AlgorithmActions()
+        {
+            steps.Add(Shot());
+            steps.Add(Jump());
+            steps.Add(Shot());
+            steps.Add(Dodge());
+            steps.Add(Shot());
+            steps.Add(Run());
+            return steps;
+        }
+
+        public string Dodge()
+        {
+            return "уворачиваться от  турели";
+        }
+
+        public string Jump()
+        {
+            return "прыгать в  турели";
+        }
+
+        public string Run()
+        {
+            return "бежать от  турели";
+        }
+
+        public string Shot()
+        {
+            return "стрелять в  турель";
+        }
+    }
+
+    class Context
+    {
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void Logica()
+        {
+
+            foreach (var item in _strategy.AlgorithmActions() as List<string>)
+            {
+                System.Console.WriteLine(item);
+            }
+            System.Console.WriteLine("**********************************");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Нападение дракона на персонажа....\n " +
+                "Применен алгоритм защиты от дракона");
+            Context context = new Context(new DragonBattleStrategy());
+            context.Logica();
+
+            System.Console.WriteLine("Нападение ГИГАНТА на персонажа....\n " +
+                "Применен алгоритм защиты от ГИГАНТА");
+            context = new Context(new GiantBattleStrategy());
+            context.Logica();
+
+            System.Console.WriteLine("Нападение Турели на персонажа....\n " +
+                "Применен алгоритм защиты от Турели");
+            context = new Context(new TurretBattleStrategy());
+            context.Logica();
+        }
+    }
+}
